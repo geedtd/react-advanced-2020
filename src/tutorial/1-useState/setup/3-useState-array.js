93e4f7daf6beb45a3fc6427e706d870176690667
geedtd <grcazares@gmail.com>
2022-05-20 23:08:16 -0700

    done with 3-useState array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,31 @@
+import { logDOM } from '@testing-library/react';
 import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data)
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+
+  return <>
+    {
+      people.map((person) => {
+        const {id, name} = person
+        return (
+        <div key={id} className='item'>
+          <h4>{name}</h4>
+          <button onClick={() => removeItem(id)}>Delete Me</button>
+        </div>
+        )
+      })
+    }
+    <button className='btn' onClick={() => setPeople([])}>
+      Delete Data
+    </button>
+  </>
 };
 
 export default UseStateArray;
